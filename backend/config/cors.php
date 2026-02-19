85992b4fb10ffa3b958312359b66c40e3a1874d7
Yukidoki1126 <kenvergel26@gmail.com>
2026-02-19 21:53:21 +0800

    cors fix

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,5 +1,10 @@
 <?php
 
+// Parse CORS_ALLOWED_ORIGINS from environment variable
+$corsOrigins = env('CORS_ALLOWED_ORIGINS') 
+    ? explode(',', env('CORS_ALLOWED_ORIGINS')) 
+    : [];
+
 return [
     /*
     |--------------------------------------------------------------------------
@@ -11,7 +16,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter([
+    'allowed_origins' => array_filter(array_merge([
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:3000',
@@ -19,10 +24,11 @@ return [
         'http://127.0.0.1:5174',
         'http://127.0.0.1:3000',
         env('FRONTEND_URL'),
-    ]),
+    ], $corsOrigins)),
 
     'allowed_origins_patterns' => [
         '#^https://.*\.vercel\.app$#',
+        '#^https://(www\.)?legalkonect\.site$#',
     ],
 
     'allowed_headers' => ['*'],
